b0ea644c14e15ca499dbd8eeab7f1cd1e80b4036
Rodrigo Campos <mr.chaskas@gmail.com>
2013-06-12 23:37:16 -0400

    integrado fblogin minimo

--- src/me/jooks/client/HomeScreenActivity.java
+++ src/me/jooks/client/HomeScreenActivity.java
@@ -1,7 +1,9 @@
 package me.jooks.client;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
 
 public class HomeScreenActivity extends Activity {
@@ -10,6 +12,11 @@ public class HomeScreenActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
+	}
+	
+	public void fblogin(View view) {
+	    Intent intent = new Intent(this, FBLoginActivity.class);
+	    startActivity(intent);
 	}
 
 }
